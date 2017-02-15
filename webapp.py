7fddec8c6c0c3c3959dc542d8d2db04406f050c0
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-02-14 23:59:06 -0600

    usando jinja

--- webapp.py
+++ webapp.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -28,6 +28,46 @@ def echo():
 
     return response_string
 
+@app.route("/home")
+def home():
+    # jinja 2
+    return render_template("home.html", app_name="WIX", nombre=u"Héctor")
+# tarea hacer una calculadora con rutas
+
+@app.route("/bio")
+def bio():
+    params = {
+        "nombre": u"Héctor",
+        "bio": "Desarrollador Devf",
+        "edad": 27,
+        "title": "Biografía de Héctor"
+    }
+    return render_template("bio.html", **params)
+
+@app.route("/list")
+def lista():
+    lista = [
+        {"nombre": "Ximena", "apodo": "Jimmy"},
+        {"nombre": "Antonio", "apodo": u"Patrón"},
+        {"nombre": u"Julián", "apodo": "Julaian"},
+        {"nombre": u"", "apodo": "Julaian"},
+    ]
+    return render_template("lista.html", lista=lista, title="Lista de Grupo")
+
+@app.route("/alumno/<ids>")
+def get_by_apodo(ids):
+    alumnos = {
+        'ximena': {"nombre": "Ximena Ortega", "bio": "Alumna DevF" },
+        'tono': {"nombre": "Antonio Banderas", "bio": "Le dicen el Patronceto"},
+        'pablo': {"nombre": u"Pablo Velázquez", "bio": "Colorear"}
+    }
+    # buscar id en diccionario
+    alumno = alumnos.get(ids)
+    # mandar lo que encuentre a la plantilla
+
+    return id
+
+@app.route("/alumno")
 
 if __name__ == "__main__":
     app.run(debug=True)
